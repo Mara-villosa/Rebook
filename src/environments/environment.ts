493518573c20a3e1ca33f632922a9de954a9fa8b
Mara <arizagarciamar@gmail.com>
2026-03-02 17:49:35 +0100

    updated backend API url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    api:{
-        url: 'localhost/Rebook/',
-        key: 'd6o06RFU8bwKUGftmVQ2Caj9OHarGZdN'
-    }
-}
+  api: {
+    url: 'http://localhost/Rebook-Backend/',
+    key: 'd6o06RFU8bwKUGftmVQ2Caj9OHarGZdN',
+  },
+};
