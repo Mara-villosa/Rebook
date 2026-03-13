40256fed915659af1d3d95d630b48eba94b1e0a6
Mara <arizagarciamar@gmail.com>
2026-03-13 19:02:14 +0100

    Added backend API endpoints to env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,13 @@ export const environment = {
   api: {
     url: 'http://localhost/Rebook-Backend/api.php',
     key: 'd6o06RFU8bwKUGftmVQ2Caj9OHarGZdN',
+    endpoints: {
+      public: {
+        login: '/login',
+        signup: '/signup',
+        refreshToken: '/refresh',
+      },
+      private: {},
+    },
   },
 };
