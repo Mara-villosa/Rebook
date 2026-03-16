df3ab6d8af7a16cba267307df36dbd0bf30c034f
AdrianLara96 <adrianlaram@gmail.com>
2026-03-16 18:03:11 +0100

    fix: resolver la duplicación del header y footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './layout/header/header';
-import { FooterComponent } from './layout/footer/footer';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
