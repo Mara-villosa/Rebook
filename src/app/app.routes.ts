e683f7f6a5641326fea38eda9e70429c55d66cf8
Mara <arizagarciamar@gmail.com>
2026-03-20 14:21:09 +0100

    Applied auth guard only to profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,14 @@ import { authGuard } from './shared/guards/auth-guard/auth-guard';
 
 export const routes: Routes = [
   // SIN sidebar (Auth)
-  { path: 'auth/login', loadComponent: () => import('./authentication/log-in/log-in').then(m => m.LogIn) },
-  { path: 'auth/signup', loadComponent: () => import('./authentication/sign-up/sign-up').then(m => m.SignUp) },
+  {
+    path: 'auth/login',
+    loadComponent: () => import('./authentication/log-in/log-in').then((m) => m.LogIn),
+  },
+  {
+    path: 'auth/signup',
+    loadComponent: () => import('./authentication/sign-up/sign-up').then((m) => m.SignUp),
+  },
 
   // CON sidebar (públicas y privadas)
   {
@@ -13,17 +19,25 @@ export const routes: Routes = [
     component: PageWithSidebarComponent,
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
-      
+
       // (sin authGuard)
-      { path: 'home', loadComponent: () => import('./home/home').then(m => m.Home) },
-      
+      { path: 'home', loadComponent: () => import('./home/home').then((m) => m.Home) },
+
       // (con authGuard)
-      { path: 'carrito', loadComponent: () => import('./carrito/carrito').then(m => m.Carrito), canActivate: [authGuard] },
-      { path: 'perfil', loadComponent: () => import('./perfil/perfil').then(m => m.Perfil), canActivate: [authGuard] },
-      { path: 'favoritos', loadComponent: () => import('./components/favorites/favorites').then(m => m.FavoritesComponent), canActivate: [authGuard] },
-    ]
+      { path: 'carrito', loadComponent: () => import('./carrito/carrito').then((m) => m.Carrito) },
+      {
+        path: 'perfil',
+        loadComponent: () => import('./perfil/perfil').then((m) => m.Perfil),
+        canActivate: [authGuard],
+      },
+      {
+        path: 'favoritos',
+        loadComponent: () =>
+          import('./components/favorites/favorites').then((m) => m.FavoritesComponent),
+      },
+    ],
   },
 
   // Wildcard al final
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: 'home' },
 ];
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,4 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { BackgroundRefreshToken } from '../shared/services/background-refresh-token-service/background-refresh-token';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +6,4 @@ import { BackgroundRefreshToken } from '../shared/services/background-refresh-to
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home implements OnInit, OnDestroy {
-  #backgroundRefreshToken = inject(BackgroundRefreshToken);
-
-  ngOnInit(): void {
-    //Mientras estamos en /home se comprueba la validez de los tokens de auth y si se necesitan refrescar
-    this.#backgroundRefreshToken.start();
-  }
-
-  ngOnDestroy(): void {
-    //Al salir de /home se detiene la comprobación de tokens
-    this.#backgroundRefreshToken.stop();
-  }
-}
+export class Home {}
