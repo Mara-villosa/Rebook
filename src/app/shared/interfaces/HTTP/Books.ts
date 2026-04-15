93e5d9be191ef2c6b46ef8d709d26c0a0d3d74c7
Mara <arizagarciamar@gmail.com>
2026-04-15 22:55:05 +0200

    feat(books endpoints): endpoint /books/getFromUser

--- src/app/shared/interfaces/HTTP/Books.ts
+++ src/app/shared/interfaces/HTTP/Books.ts
@@ -1,3 +1,4 @@
+// /books/new
 export interface UploadBookRequest {
   title: string;
   description: string;
@@ -9,22 +10,35 @@ export interface UploadBookRequest {
   sell_price?: number;
 }
 
+// /books/delete
 export interface DeleteBookRequest {
   book_id: number;
 }
 
-export interface GetAllBooksRequest {
-  books: {
-    id: number;
-    title: string;
-    author: string;
-    description: string;
-    rentPrice: number;
-    sellPrice: number;
-    isbn: string;
-    url: string;
-    category: string;
-    rented: boolean;
-    inCart: boolean;
-  }[];
+// /books/getAll
+export interface GetAllBooksResponse {
+  books: BookDTO[];
+}
+
+// /books/getFromUser
+export interface GetAllBooksFromUserResponse {
+  uploads: BookDTO[];
+  rented: BookDTO[];
+  bought: BookDTO[];
+}
+interface BookDTO {
+  id: number;
+  title: string;
+  author: string;
+  description: string;
+  rentPrice: number;
+  sellPrice: number;
+  isbn: string;
+  url: string;
+  category: string;
+  rented: boolean;
+  inCart: boolean;
+  id_user: number;
+  rent_expired: boolean;
+  sold: boolean;
 }
