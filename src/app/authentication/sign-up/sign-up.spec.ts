af37debc3cabb19de36da32d68aecb57c0da6b7c
Mara <arizagarciamar@gmail.com>
2026-04-21 09:28:09 +0200

    feat(testing): added support for karma + jasmine unit testing

--- src/app/authentication/sign-up/sign-up.spec.ts
+++ src/app/authentication/sign-up/sign-up.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../app.routes';
 import { SignUp } from './sign-up';
 
 describe('SignUp', () => {
@@ -8,9 +11,9 @@ describe('SignUp', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SignUp]
-    })
-    .compileComponents();
+      imports: [SignUp],
+      providers: [provideHttpClient(), provideRouter(routes)],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SignUp);
     component = fixture.componentInstance;
